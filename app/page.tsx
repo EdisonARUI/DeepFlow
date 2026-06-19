c9e793e1c9cfa48bd1a316d0dab8d4878b20c6da
EdisonRUI <zrui0761@gmail.com>
2026-06-19 22:23:18 +0900

    feat: Add LandingHeader component to enhance page structure
    - Introduced the `LandingHeader` component to the main page for improved layout and user navigation.
    - Updated the main component to include the new header, enhancing the overall user experience.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { LandingFooter } from "./_components/landing-footer";
+import { LandingHeader } from "./_components/landing-header";
 import { LandingHero } from "./_components/landing-hero";
 import { LandingPartners } from "./_components/landing-partners";
 import { LandingProducts } from "./_components/landing-products";
@@ -6,6 +7,7 @@ import { LandingProducts } from "./_components/landing-products";
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#020617]">
+      <LandingHeader />
       <LandingHero />
       <LandingProducts />
       <LandingPartners />
